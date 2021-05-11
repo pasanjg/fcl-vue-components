c7cac9d7a26522e1fe981e51cc13004767d4a57b
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-05-12 00:28:06 +0530

    Setup routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import
-
-	new Vue({
-		data: () => ({ message: 'Row' }),
-		template: `
-	<div>
-		<h1>{{message}} your boat</h1>
-		<button v-on:click="message += ' row'">Add</button>
-	</div>
-	`
-	}).$mount('#root');
+const Index = {
+  template: `
+      <h1>Hello World!</h1>
+  `,
+  data() {
+    return {
+      indexMessage: "Index page loaded",
+    };
+  },
+};
