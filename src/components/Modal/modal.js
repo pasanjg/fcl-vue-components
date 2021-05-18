da9a4b1ff0bfd1902ba572bea7b2050ef50f01e2
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-05-18 12:14:38 +0530

    Modal draft

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,9 +1,56 @@
 Vue.component('fv-modal', {
-  props: ['content-class'],
+  props: ['contentclass', 'title', 'modalid', 'buttonid', 'center'],
   data: function () {
     return {
-      count: 0
+      id: undefined,
+      centerclass: '',
     }
   },
-  template: '<button v-on:click="count++">You clicked Modal {{ count }} times.</button>'
+  mounted() {
+    this.id = this.modalid;
+    this.centerclass = this.center ? 'modal-dialog-centered' : '';
+    this.openModal(this.modalid, this.buttonid);
+    this.closeModal(this.modalid);
+  },
+  methods: {
+    openModal: function (modalid, buttonid) {
+      var modal = document.getElementById(modalid);
+
+      document.getElementById(buttonid).addEventListener("click", function (e) {
+        modal.style.display = "block";
+        modal.className = "modal fade show";
+      });
+    },
+    closeModal: function (modalid) {
+      var modal = document.getElementById(modalid);
+      var close = document.getElementById('modalClose');
+
+      close.addEventListener('click', (e) => {
+        modal.style.display = "none";
+        modal.className = "modal fade";
+      });
+    }
+  },
+  template: `
+  <div>
+    <div class="modal fade" v-bind:id="modalid" tabindex="-1">
+      <div v-bind:class="['modal-dialog', centerclass]">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{ title }}</h5>
+            <button id="modalClose" type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
+          <div class="modal-footer">
+            <slot name="footer"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
 });
--- src/views/modalView.js
+++ src/views/modalView.js
@@ -1,5 +1,5 @@
 const ModalView = {
-	props: ['content-class'],
+	props: ['contentclass'],
 	data() {
 		return {
 			message: "",
@@ -8,7 +8,20 @@ const ModalView = {
 	template: `
 		<div>
 			<h3>Modal</h3>
-			<fv-modal/>
+
+			<button id="demoTrigger" type="button" class="btn btn-primary" data-toggle="modal" data-target="#demoModal">
+      	Launch demo modal
+    	</button>
+
+			<fv-modal modalid="demoModal" buttonid="demoTrigger" title="Demo Modal" center="true">
+			<template v-slot:body>
+				<p>Modal content goes here</p>
+			</template>
+			<template v-slot:footer>
+				<button id="modalClose" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</template>
+			</fv-modal>
 		</div>
   `,
 };
