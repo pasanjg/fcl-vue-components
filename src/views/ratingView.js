7dacda27328d2d27e9f0843bf9312d64812ca455
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-06-16 02:04:34 +0530

    Add custom star count view

--- src/views/ratingView.js
+++ src/views/ratingView.js
@@ -22,13 +22,18 @@ const RatingView = {
     <h3>Rating</h3>
     <br />
     <h5>Fixed value</h5>
-      <fv-rating rateId="demoRating" contentclass="d-inline" v-bind:value="updatedRating"/> <span>{{ updatedRating }}</span> <br />
-      <fv-rating rateId="demoRating2" contentclass="d-inline" v-bind:value="updatedRating2"/> <span>{{ updatedRating2 }}</span> <br />
+    <fv-rating rateId="demoRating" contentclass="d-inline" v-bind:value="updatedRating"/> <span>{{ updatedRating }}</span> <br />
+    <fv-rating rateId="demoRating2" contentclass="d-inline" v-bind:value="updatedRating2"/> <span>{{ updatedRating2 }}</span> <br />
     <br />
 
     <h5>Editable</h5>
-      <fv-rating rateId="demoRating3" count="3" contentclass="d-inline" v-on:demoRating3="updateRating3" canupdate="true" /> <span>{{ updatedRating3 }}</span> <br />
-      <fv-rating rateId="demoRating4" count="6" contentclass="d-inline" v-on:demoRating4="updateRating4" canupdate="true" /> <span>{{ updatedRating4 }}</span> <br />
+    <fv-rating rateId="demoRating3"  contentclass="d-inline" v-on:demoRating3="updateRating3" canupdate="true" /> <span>{{ updatedRating3 }}</span> <br />
+    <fv-rating rateId="demoRating4" contentclass="d-inline" v-on:demoRating4="updateRating4" canupdate="true" /> <span>{{ updatedRating4 }}</span> <br />
+    <br />
+
+    <h5>Custom count</h5>
+    <fv-rating rateId="demoRating5" count="3" contentclass="d-inline" v-bind:value="updatedRating"/> <span>{{ updatedRating }}</span> <br />
+    <fv-rating rateId="demoRating6" count="6" contentclass="d-inline" v-bind:value="updatedRating2"/> <span>{{ updatedRating2 }}</span> <br />
   </div>
   `,
 };
