e14e9f484fca3142b9b2e3514f72cf550fb05c5f
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-06-26 06:26:18 +0530

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
 module.exports = {
   mode: 'production',
   entry: {
-    fv: `./src/index.js`,
+    fv: './src/index.js',
+    styles: './assets/scss/styles.scss',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-  }
+    filename: 'js/[name].bundle.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'css/[name].min.css'
+        },
+        use: ['sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin({ extensions: ['scss'] }),
+  ],
 };
