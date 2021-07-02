7878e2b22ed990df005962503c9c8751d6b9ffc4
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-07-02 20:43:03 +0530

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
 const path = require('path');
 const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    fv: './src/index.js',
-    styles: './assets/scss/styles.scss',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].bundle.min.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'css/[name].min.css'
+module.exports = (env) => {
+  const mode = env && env.mode;
+  const outputPath = mode === 'production' ?
+    path.resolve(__dirname, 'dist')
+    : path.resolve(__dirname, 'build');
+
+  return {
+    mode: mode || 'development',
+    entry: {
+      fv: './src/index.js',
+      styles: './assets/scss/styles.scss',
+    },
+    output: {
+      path: outputPath,
+      filename: 'js/[name].bundle.min.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'css/[name].min.css'
+          },
+          use: ['sass-loader'],
         },
-        use: ['sass-loader'],
-      },
+      ],
+    },
+    plugins: [
+      new FixStyleOnlyEntriesPlugin({ extensions: ['scss'] }),
     ],
-  },
-  plugins: [
-    new FixStyleOnlyEntriesPlugin({ extensions: ['scss'] }),
-  ],
+  }
 };
