9c6bc922b342712036ba90b23084b1ec516743e6
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-08-04 10:05:10 +0530

    Select2 watch dataList changes

--- src/views/select2View.js
+++ src/views/select2View.js
@@ -1,47 +1,37 @@
 export const Select2View = {
   data: function () {
     return {
-      colors: [
-        {
-          "value": "red",
-          "displayName": "Red",
-        },
-        {
-          "value": "green",
-          "displayName": "Green",
-        },
-        {
-          "value": "purple",
-          "displayName": "Purple",
-        },
-        {
-          "value": "orange",
-          "displayName": "Orange",
-        },
-        {
-          "value": "black",
-          "displayName": "Black",
-        },
-        {
-          "value": "violet",
-          "displayName": "Violet",
-        },
-        {
-          "value": "blue",
-          "displayName": "Blue",
-        },
+      types: [
+        { "value": "todos", "displayName": "Todos" },
+        { "value": "users", "displayName": "Users" },
       ],
+      choices: [],
+      choiceDataDisplay: null,
+      colors: [
+        { "value": "red", "displayName": "Red" },
+        { "value": "green", "displayName": "Green" },
+        { "value": "purple", "displayName": "Purple" },
+        { "value": "orange", "displayName": "Orange" },
+        { "value": "black", "displayName": "Black" },
+        { "value": "violet", "displayName": "Violet" },
+        { "value": "blue", "displayName": "Blue" },
+      ],
+      pets: [
+        { "value": "cat", "displayName": "Cat" },
+        { "value": "dog", "displayName": "Dog" },
+        { "value": "fish", "displayName": "Fish" },
+        { "value": "bird", "displayName": "Bird" },
+      ],
+
       selected: {},
-      selected2: {
-        "value": "red",
-        "displayName": "Red",
-      },
-      selected3: {
-        "value": "green",
-        "displayName": "Green",
-      },
+      selected2: { "value": "cat", "displayName": "Cat" },
+      selected3: { "value": "red", "displayName": "Red" },
+
+      type: {},
+      choice: {},
     }
   },
+  inject: ['fetcher'],
   methods: {
     getNewItem(item) {
       console.debug('Added', item);
@@ -49,12 +39,46 @@ export const Select2View = {
     getRemovedItem(item) {
       console.debug('Removed', item);
     },
+    async setChoiceList(type) {
+      this.choice = {};
+      if (type['value'] === 'todos') {
+        const url = "https://jsonplaceholder.typicode.com/todos"
+        this.choiceDataDisplay = 'title'
+        return await this.fetcher(url);
+
+      } else if (type['value'] === 'users') {
+        const url = "https://jsonplaceholder.typicode.com/users"
+        this.choiceDataDisplay = 'name'
+        return await this.fetcher(url);
+      }
+    }
+  },
+  watch: {
+    async type(newValue, oldValue) {
+      this.choices = await this.setChoiceList(newValue);
+    },
+    choice(newValue, oldValue) {
+
+    },
   },
   template:
     `
 		<div>
 			<h3>Select2</h3>
       <br />
+
+      <h6>Select a type and choice</h6>
+      <div class="row">
+        <div class="col">
+          {{type}}
+          <fv-select2 id="demoTypes" :data-list="types" data-display="displayName" data-value="value" v-model="type" placeholder="Select type"/>
+        </div>
+        <div class="col">
+          {{choice}}
+          <fv-select2 id="demoChoice" :data-list="choices" :data-display="choiceDataDisplay" data-value="id" v-model="choice" placeholder="Select Choice"/>
+        </div>
+      </div>
+      <br /> <br />
 
       <h6><code>id="demoSelect" :data-list="[{value: 'red', displayName: 'Red'}, {value: 'blue', displayName: 'Blue'}]" data-display="displayName" data-value="value" v-model="selected"</code></h6>
       <small>selected: {{selected}}</small>
@@ -64,13 +88,13 @@ export const Select2View = {
       <h6>Add a new value which is not included in the list</h6>
       <h6><code>allow-new="true" @onAdd="getNewItem"</code></h6>
       <small>selected: {{selected2}}</small>
-			<fv-select2 id="demoSelect3" :data-list="colors" data-display="displayName" data-value="value" v-model="selected2" allow-new="true" @onAdd="getNewItem" placeholder="Select color"/>
+			<fv-select2 id="demoSelect2" :data-list="pets" data-display="displayName" data-value="value" v-model="selected2" allow-new="true" @onAdd="getNewItem" placeholder="Select"/>
       <br /> <br />
 
       <h6>Remove a value from the list</h6>
       <h6><code>allow-remove="true" @onRemove="getRemovedItem"</code></h6>
       <small>selected: {{selected3}}</small>
-			<fv-select2 id="demoSelect4" :data-list="colors" data-display="displayName" data-value="value" v-model="selected3" allow-remove="true" @onRemove="getRemovedItem" placeholder="Select color"/>
+			<fv-select2 id="demoSelect3" :data-list="colors" data-display="displayName" data-value="value" v-model="selected3" allow-remove="true" @onRemove="getRemovedItem" placeholder="Select color"/>
       <br /> <br />
 		</div>
   `,
