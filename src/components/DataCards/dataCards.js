c4bd2dad1c6817b10b9e4a54bc688207e9fff33a
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-08-12 10:18:13 +0530

    DataCards DRAFT

--- src/components/DataCards/dataCards.js
+++ src/components/DataCards/dataCards.js
@@ -1,9 +1,44 @@
 export const FvDataCards = {
-  props: ['className'],
+  props: {
+    id: {
+      type: String,
+    },
+    className: {
+      type: String,
+      default: '',
+    },
+    col: {
+      type: Number,
+      default: 3,
+      validator: function (value) {
+        return [1, 2, 3, 4, 6, 12].indexOf(value) !== -1
+      },
+    },
+    dataList: {
+      type: Array,
+      default: () => ['<p>Description</p>', 1960, 1970, 1980, 1990, 2000, 2010],
+    },
+    dataTemplate: {
+      type: String,
+      default: '<p>Add your template</p>',
+    },
+  },
   data: function () {
     return {
-      count: 0
+      title: 'Title',
+      description: 'Description',
+      year: 2021,
+      isSelected: false,
     }
   },
-  template: '<button v-on:click="count++">You clicked DataCards {{ count }} times.</button>'
+  template:
+    `
+    <div>
+      <div class="row">
+        <div :class="['col-md-'+col, 'mb-3', className]" v-for="data in dataList">
+          <slot/>
+        </div>
+      </div>
+    </div>
+  `
 };
--- src/views/dataCardsView.js
+++ src/views/dataCardsView.js
@@ -1,10 +1,31 @@
 export const DataCardsView = {
+  inject: ['fetcher'],
+  data: function () {
+    return {
+      dataTemplate: `
+      <div class="card">
+        <div class="card-header">
+          <i class="fab fa-vuejs"></i>
+          <span></span>
+        </div>
+        <div class="card-body">
+          {{data}}
+        </div>
+      </div>
+      `,
+    }
+  },
+  methods: {
+
+  },
   template:
     `
 		<div>
 			<h3>DataCards</h3>
       <br />
-			<fv-datacards/>
+			<fv-datacards :data-template="dataTemplate">
+        <p>dsdsds</p>
+      <fv-datacards/>
 		</div>
   `,
 };
