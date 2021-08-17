9014b6f0d30efeb37ac9059247ee4bbe2f869c92
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-08-17 23:44:43 +0530

    Setup Select2 Multiselect

--- src/components/Select2/select2.js
+++ src/components/Select2/select2.js
@@ -1,4 +1,4 @@
-import { AutoClose } from "../vue-directives/vue-auto-close.js";
+import { AutoClose } from "../../directives/autoClose.js";
 
 export const FvSelect2 = {
   model: {
@@ -6,7 +6,9 @@ export const FvSelect2 = {
     event: 'onSelect'
   },
   props: {
-    className: String,
+    className: {
+      type: String,
+    },
     id: {
       type: String,
       required: true,
@@ -33,13 +35,16 @@ export const FvSelect2 = {
       type: String,
       default: 'Select',
     },
-    allowNew: {
+    multiSelect: {
       type: Boolean,
       default: false,
     },
-    multiSelect: {
-        type: Boolean,
-        default: false,
+    multiSelectKey: {
+      type: String,
+    },
+    allowNew: {
+      type: Boolean,
+      default: false,
     },
     allowRemove: {
       type: Boolean,
@@ -51,8 +56,9 @@ export const FvSelect2 = {
       customInputValue: null,
       selectRef: `${this.id}SelectRef`,
       toggleButtonRef: `${this.id}ToggleButtonRef`,
-        filterInputRef: `${this.id}FilterInputRef`,
-        allSelected: false
+      filterInputRef: `${this.id}FilterInputRef`,
+      allSelected: false,
+      multiSelected: [],
     }
   },
   mounted: function () {
@@ -69,6 +75,10 @@ export const FvSelect2 = {
 
       customField.style.display = "none";
 
+      if (vm.multiSelect || vm.multiSelect == "true") {
+        vm.multiSelected = this.dataList.filter(item => item[this.multiSelectKey] === true);
+      }
+
       this.renderList(this.dataList);
       this.filterList(filterInput);
 
@@ -77,25 +87,28 @@ export const FvSelect2 = {
           if (filterInput.value != null && filterInput.value != "") {
             filterInput.value = null;
           }
+
           vm.renderList(vm.dataList);
           menu.classList['toggle']('show');
         });
       });
     },
-      renderList: function (dataList) {
-
+    getValue() {
+      if (this.multiSelect || this.multiSelect == "true") {
+        this.emitToVModel(this.multiSelected);
+        return this.value = this.multiSelected.map(item => item[this.dataDisplay]).join(', ');
+      } else {
+        return this.value[this.dataDisplay];
+      }
+    },
+    renderList: function (dataList) {
       const vm = this;
-      const menuList = document.querySelector(`#${this.id}MenuList`);
+      const menuList = document.querySelector(`#${vm.id}MenuList`);
       menuList.innerHTML = "";
 
       for (let index in dataList) {
         const menuItem = document.createElement("div");
         const selectItem = document.createElement("div");
-
-        const checkbox = document.createElement("input");
-        checkbox.setAttribute("class", 'mr-2');
-        checkbox.setAttribute("type", 'checkbox');
-
 
         menuItem.setAttribute("class", 'dropdown-item');
         menuItem.classList.add("d-flex", "justify-content-between", "align-items-center");
@@ -106,24 +119,42 @@ export const FvSelect2 = {
         selectItem.style.paddingTop = '0.25rem';
         selectItem.style.paddingBottom = '0.25rem';
         menuItem.style.cursor = 'pointer';
-        selectItem.innerHTML = dataList[index][this.dataDisplay];
+        selectItem.innerHTML = dataList[index][vm.dataDisplay];
 
-          if (this.multiSelect) {
-              checkbox.setAttribute("value", dataList[index][this.dataValue]);
-              var itemInOriginalList = vm.dataList.find(e => e.Id == dataList[index][this.dataValue]);
-              if (itemInOriginalList?.isActive) {
-                  checkbox.setAttribute("checked", "checked");
-              }
-              else
-                  checkbox.removeAttribute("checked");
+        if (vm.multiSelect || vm.multiSelect == "true") {
+          const checkbox = document.createElement("input");
 
-              menuItem.appendChild(checkbox);
-              checkbox.addEventListener("click", function (e) {
-                  vm.setAsSelected(vm.dataList[index],e);
-                  //vm.closeMenu();
-              });
+          checkbox.setAttribute("id", `${vm.id + vm.dataList[index][vm.dataValue]}CheckBox`);
+          checkbox.setAttribute("class", 'mr-2');
+          checkbox.setAttribute("type", 'checkbox');
+          checkbox.setAttribute("value", dataList[index][vm.dataValue]);
+
+          let itemInOriginalList = vm.dataList.find(element => element[vm.dataValue] == dataList[index][vm.dataValue]);
+
+          if (itemInOriginalList && itemInOriginalList[vm.multiSelectKey]) {
+            checkbox.checked = true;
+          }
+          else {
+            checkbox.checked = false;
           }
 
+          menuItem.appendChild(checkbox);
+          checkbox.addEventListener("click", function (event) {
+            vm.setAsSelected(vm.dataList[index], event.target);
+          });
+        }
+
+        if (vm.allowRemove || vm.allowRemove == "true") {
+          const removeBtn = document.createElement("i");
+          removeBtn.classList.add("fa", "fa-times", "text-muted", "btn", "btn-light", "px-2", "mr-2");
+          removeBtn.style.cursor = "pointer";
+          menuItem.appendChild(removeBtn);
+
+          removeBtn.addEventListener("click", function (e) {
+            vm.removeFromList(vm.dataList[index]);
+            vm.closeMenu();
+          });
+        }
 
         menuItem.appendChild(selectItem);
         menuList.appendChild(menuItem);
@@ -131,34 +162,8 @@ export const FvSelect2 = {
         menuList.style.maxHeight = "12rem";
         menuList.style.overflowY = "scroll";
 
-        if (vm.allowRemove || vm.allowRemove == "true") {
-          const removeBtn = document.createElement("i");
-          removeBtn.classList.add("float-right", "fa", "fa-times", "text-muted", "btn", "btn-light", "px-2");
-          removeBtn.style.cursor = "pointer";
-          menuItem.appendChild(removeBtn);
-
-          removeBtn.addEventListener("click", function (e) {
-            vm.removeSelectedValue(vm.dataList[index]);
-            vm.closeMenu();
-          });
-        }
-
         this.handleSelect(selectItem, dataList[index]);
       }
-    },
-    handleSelect: function (selectItem, selected) {
-      const vm = this;
-      const selectInput = document.getElementById(vm.id);
-
-      selectItem.addEventListener(('click'), function () {
-        selectInput.value = selected[vm.dataDisplay];
-        vm.updateSelectedValue(selected);
-
-        setTimeout(() => {
-          vm.renderList(vm.dataList);
-          vm.closeMenu();
-        }, 100);
-      });
     },
     filterList: function (filterInput) {
       const vm = this;
@@ -172,7 +177,7 @@ export const FvSelect2 = {
 
         vm.renderList(filteredOptions);
 
-        if (vm.allowNew || vm.allowNew == "true") {
+        if (vm.allowNew || vm.allowNew === "true") {
           if (filterInput.value != null && filterInput.value != "") {
             vm.customInputValue = filterInput.value;
             customField.style.display = "block";
@@ -181,7 +186,7 @@ export const FvSelect2 = {
           }
 
           if (filteredOptions.length >= 1) {
-            if (filteredOptions[0][vm.dataDisplay].toLowerCase() == filterInput.value.toLowerCase()) {
+            if (filteredOptions[0][vm.dataDisplay].toLowerCase() === filterInput.value.toLowerCase()) {
               customField.style.display = "none";
             }
           }
@@ -192,13 +197,71 @@ export const FvSelect2 = {
 
       vm.handleCustomInput();
     },
+    handleSelect: function (selectItem, selectedValue) {
+      const vm = this;
+      const selectInput = document.getElementById(vm.id);
+
+      selectItem.addEventListener(('click'), function () {
+
+        if (vm.multiSelect || vm.multiSelect == "true") {
+          const checkbox = document.getElementById(`${vm.id + selectedValue[vm.dataValue]}CheckBox`);
+          checkbox.checked = !checkbox.checked;
+
+          vm.dataList = vm.dataList.map(item => {
+            if (item[vm.dataValue] == selectedValue[vm.dataValue]) {
+              item[vm.multiSelectKey] = checkbox.checked;
+
+              if (checkbox.checked) {
+                vm.multiSelected.push(item);
+              } else {
+                vm.multiSelected = vm.multiSelected.filter(element => element[vm.dataValue] != selectedValue[vm.dataValue]);
+              }
+            }
+            return item;
+          });
+
+        } else {
+          selectInput.value = selectedValue[vm.dataDisplay];
+          vm.emitToVModel(selectedValue);
+          setTimeout(() => {
+            vm.renderList(vm.dataList);
+            vm.closeMenu();
+          }, 100);
+        }
+      });
+    },
+    setAsSelected(selectedValue, checkbox) {
+      const vm = this;
+
+      vm.dataList = vm.dataList.map(item => {
+        if (item[vm.dataValue] == selectedValue[vm.dataValue]) {
+          item[vm.multiSelectKey] = checkbox.checked;
+
+          if (checkbox.checked) {
+            vm.multiSelected.push(item);
+          } else {
+            vm.multiSelected = vm.multiSelected.filter(item => item[vm.dataValue] != selectedValue[vm.dataValue]);
+          }
+        }
+        return item;
+      });
+    },
+    selectAll(event) {
+      var vm = this;
+      vm.dataList.forEach(element => element[vm.multiSelectKey] = event.target.checked);
+      if (event.target.checked) {
+        vm.multiSelected = vm.dataList;
+      } else {
+        vm.multiSelected = [];
+      }
+      this.renderList(vm.dataList);
+    },
     handleCustomInput: function () {
       const vm = this;
       const customField = document.getElementById(`${vm.id}CustomField`);
 
       customField.addEventListener('click', function () {
-
-        vm.addSelectedValue(vm.customInputValue);
+        vm.addNewToList(vm.customInputValue);
 
         setTimeout(() => {
           customField.style.display = "none";
@@ -206,6 +269,15 @@ export const FvSelect2 = {
           vm.closeMenu();
         }, 100);
       });
+    },
+    emitToVModel(value) {
+      this.$emit('onSelect', value);
+    },
+    addNewToList(selectedValue) {
+      this.$emit('onAdd', selectedValue);
+    },
+    removeFromList(selectedValue) {
+      this.$emit('onRemove', selectedValue);
     },
     closeMenu: function () {
       const vm = this;
@@ -221,40 +293,6 @@ export const FvSelect2 = {
           vm.renderList(this.dataList);
         }
       }
-    },
-    getValue() {
-          return this.value[this.dataDisplay];
-    },
-    updateSelectedValue(selectedValue) {
-      let selected = {
-        [this.dataDisplay]: selectedValue[this.dataDisplay],
-        [this.dataValue]: selectedValue[this.dataValue],
-      }
-      this.$emit('onSelect', selected);
-    },
-    addSelectedValue(selectedValue) {
-      this.$emit('onAdd', selectedValue);
-    },
-    setAsSelected(selectedValue, ele) {
-        const vm = this;
-        //var item = vm.dataList.find(e => e.Id == ele.target.value);
-        //var idx = vm.dataList.findIndex(e => e.Id == ele.target.value);
-        //item.isActive = ele.target.checked;
-        //vm.dataList[idx] = item;
-        _.set(vm.dataList.find(e => e.Id == ele.target.value), 'isActive', ele.target.checked);
-    },
-      selectAll(evt) {
-        var vm = this;
-          vm.dataList.forEach(e => e.isActive = evt.target.checked);
-          this.renderList(vm.dataList);
-    },
-    removeSelectedValue(selectedValue) {
-      let selected = {
-        [this.dataDisplay]: selectedValue[this.dataDisplay],
-        [this.dataValue]: selectedValue[this.dataValue],
-      }
-
-      this.$emit('onRemove', selected);
     },
   },
   watch: {
@@ -275,8 +313,8 @@ export const FvSelect2 = {
         <button type="button" :ref="toggleButtonRef" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split shadow-none" :data-target="id" data-toggle="dropdown">
         </button>
         <div :key="id" class="w-100 dropdown-menu" :data-menu="id" v-auto-close="{ exclude: [selectRef, toggleButtonRef, filterInputRef], handler: 'closeMenu' }">
-          <label v-if="multiSelect" class="ml-3"><input class="mr-2" type="checkbox" v-on:click="selectAll($event)" >Select all</label>
-          <label v-if="allowRemove" class="float-right mr-3"><input class="mr-2" type="checkbox">Remove all</label>
+          <span v-if="multiSelect" class="btn btn-sm btn-light px-2 mb-2 ml-3"><input class="mr-2" type="checkbox" v-on:click="selectAll($event)" >Select all</span>
+          <span v-if="allowRemove" class="btn btn-sm btn-danger float-right px-2 mr-2 mb-2">Remove all</span>
           <input type="search" :ref="filterInputRef" class="form-control shadow-none mx-auto mb-2" :data-filter="id" placeholder="Filter" style="width: 95%" />
           <span :id="id+'CustomField'" class="dropdown-item">
             <i class="fa fa-plus text-muted"></i>
