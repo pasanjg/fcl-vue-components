6243e45dabdc776433596a9f58846067e819f15e
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-08-18 14:34:33 +0530

    Update App and Documentation

--- src/views/select2View.js
+++ src/views/select2View.js
@@ -82,7 +82,7 @@ export const Select2View = {
 			<h3>Select2</h3>
       <br />
 
-      <h6>Select a type and choice</h6>
+      <h6>Demo</h6>
       <div class="row">
         <div class="col"><small>{{type}}</small></div>
         <div class="col"><small>{{choice}}</small></div>
@@ -103,31 +103,56 @@ export const Select2View = {
       <br /> <br />
 
       <h6>Basic usage</h6>
-      <h6><code>id="demoSelect" :data-list="[{value: 'red', displayName: 'Red'}, {value: 'blue', displayName: 'Blue'}]" data-display="displayName" data-value="value" v-model="selected"</code></h6>
+      <h6>
+        <pre><code>&lt;fv-select2 id="demoSelect" :data-list="[{value: 'red', displayName: 'Red'}, {value: 'blue', displayName: 'Blue'}]" data-display="displayName" data-value="value" v-model="selected" /&gt;</code></pre>
+      </h6>
       <small>selected: {{selected}}</small> <br /> <br />
 			<fv-select2 id="demoSelect" :data-list="colors" data-display="displayName" data-value="value" v-model="selected" placeholder="Select color"/>
       <br /> <br />
 
-      <h6>Multiselect</h6>
-      <h6><code>multi-select="true" multi-select-key="isSelected"</code></h6>
-      <small>selected: {{selectedMulti}}</small> <br /> <br />
-      <small>{{selectedMulti.length}} items selected</small> <br /> <br />
-			<fv-select2 id="demoMulti" multi-select="true" multi-select-key="isSelected" :data-list="colorsMulti" data-display="displayName" data-value="value" v-model="selectedMulti" allow-new="true" @onAdd="getNewItem" allow-remove="true" @onRemove="getRemovedItem" @onRemoveAll="getAllRemovedItems" placeholder="Select color"/>
-      <br /> <br />
-
       <h6>Add a new value which is not included in the list</h6>
       <h6><code>allow-new="true" @onAdd="getNewItem"</code></h6>
+      <textarea class="code-snippet" rows="4">
+      getNewItem(item) {
+        // Handle the added item
+        console.debug('Added', item);
+      }</textarea> <br /> <br />
       <small>selected: {{selected2}}</small> <br /> <br />
 			<fv-select2 id="demoSelect2" :data-list="pets" data-display="displayName" data-value="value" v-model="selected2" allow-new="true" @onAdd="getNewItem" placeholder="Select"/>
       <br /> <br />
 
       <h6>Remove a value from the list</h6>
       <h6><code>allow-remove="true" @onRemove="getRemovedItem" @onRemoveAll="getAllRemovedItems"</code></h6>
+      <textarea class="code-snippet" rows="9">
+      getRemovedItem(item) {
+        // Handle removed item
+        console.debug('Removed', item);
+      }
+
+      getAllRemovedItems(items) {
+        // Handle all removed items
+        console.debug('All Removed', items);
+      },</textarea> <br /> <br />
       <small>selected: {{selected3}}</small> <br /> <br />
 			<fv-select2 id="demoSelect3" :data-list="colors" data-display="displayName" data-value="value" v-model="selected3" allow-remove="true" @onRemove="getRemovedItem" @onRemoveAll="getAllRemovedItems" placeholder="Select color"/>
       <br /> <br />
 
-
+      <h6>Multiselect</h6>
+      <h6><code>:data-list="colorsMulti" multi-select="true" multi-select-key="isSelected"</code></h6>
+      <textarea class="code-snippet" rows="9">
+      colorsMulti: [
+        { "value": "red", "displayName": "Red", "isSelected": true },
+        { "value": "green", "displayName": "Green", "isSelected": true },
+        { "value": "purple", "displayName": "Purple", "isSelected": false },
+        { "value": "orange", "displayName": "Orange", "isSelected": false },
+        { "value": "black", "displayName": "Black", "isSelected": true },
+        { "value": "violet", "displayName": "Violet", "isSelected": false },
+        { "value": "blue", "displayName": "Blue", "isSelected": false },
+      ],</textarea> <br /> <br />
+      <small>selected: {{selectedMulti}}</small> <br /> <br />
+      <small>{{selectedMulti.length}} items selected</small> <br /> <br />
+			<fv-select2 id="demoMulti" multi-select="true" multi-select-key="isSelected" :data-list="colorsMulti" data-display="displayName" data-value="value" v-model="selectedMulti" allow-new="true" @onAdd="getNewItem" allow-remove="true" @onRemove="getRemovedItem" @onRemoveAll="getAllRemovedItems" placeholder="Select color"/>
+      <br /> <br />
 		</div>
   `,
 };
