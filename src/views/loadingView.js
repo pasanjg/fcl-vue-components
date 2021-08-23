a8f125dd20a2e6b5cec93c4e34a78ca03da242cd
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-08-23 10:15:33 +0530

    Update loading view

--- src/views/loadingView.js
+++ src/views/loadingView.js
@@ -7,19 +7,21 @@ export const LoadingView = {
   },
   inject: ['fetcher'],
   mounted: function () {
-    setTimeout(() => {
-      this.getData();
-    }, 3000);
+    this.getData();
   },
   methods: {
     async getData() {
       const vm = this;
-      const url = "https://jsonplaceholder.typicode.com/users"
-      await this.fetcher(url).then(data => {
+      this.isLoading = true;
+      const urls = ["https://jsonplaceholder.typicode.com/users", "https://jsonplaceholder.typicode.com/posts"];
+      const rand = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+
+      setTimeout(async () => await this.fetcher(urls[rand]).then(data => {
         vm.dataFetched = data;
         vm.isLoading = false;
         console.log(vm.isLoading);
-      });
+      }), 3000);
+
     },
   },
   template:
@@ -30,17 +32,20 @@ export const LoadingView = {
 
       <a href="https://github.com/rihanrahul/vue-tiny-loading-overlay"><h5>Docs</h5></a>
 
-      {{isLoading}} <br />
-      {{dataFetched}}
+      Loading: {{isLoading}}
+      <br /> <br />
 
       <div class="card" style="width: 18rem;" v-loading="isLoading">
         <img src="https://ytimg.googleusercontent.com/vi/b85iLY-wQXI/hqdefault.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" class="btn btn-primary" @click="getData">Load data</a>
         </div>
       </div>
+      <br />
+
+      Loaded data: {{dataFetched}}
       <br /> <br />
 		</div>
   `,
